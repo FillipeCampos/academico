a89f27e3ff0a1c8281327149167df9144d96b18d
FillipeCampos <fillipecampos19@gmail.com>
2018-11-07 14:37:55 -0300

    Avaliacao

--- app/Avaliacao.php
+++ app/Avaliacao.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Avaliacao extends Model
 {
     //
+    protected $fillable = [
+        'id',  
+        'peso',
+        'turma_id'
+      ];
+  
+      protected $table = 'turma_alunos';
+
+      public function turma()
+      {
+        return $this->belongsTo(Turma::class, 'turma_id');
+      }
 }
