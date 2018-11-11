4511a8a01975f75cb465ee54151f07a0a9455e23
SilveiraVictor <silveiravictor97@gmail.com>
2018-11-11 19:13:16 -0300

    alterações blade

---
+++ resources/views/template/head.blade.php
@@ -0,0 +1,23 @@
+<title>Academico</title>
+
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+
+<!-- FontAwesome CSS -->
+<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}" >
+
+<!-- ElegantFonts CSS -->
+<link rel="stylesheet" href="{{ asset('css/elegant-fonts.css') }}"   >
+
+<!-- themify-icons CSS -->
+<link rel="stylesheet" href="{{ asset('css/themify-icons.css') }}" >
+
+<!-- Swiper CSS -->
+<link rel="stylesheet" href="{{ asset('css/swiper.min.css') }}" >
+
+<!-- Styles -->
+<link rel="stylesheet" href="{{ asset('css/style.css') }}" >
